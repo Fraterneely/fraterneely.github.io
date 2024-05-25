34396ea62ac15f5053efec77ee93af4b2a664c07
KNOWNGroup <167290418+Fraterneely@users.noreply.github.com>
2024-05-25 17:29:07 +0200

    Create metatagsall.js

---
+++ js/metatagsall.js
@@ -0,0 +1,8 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "url": "https://www.fraterneely.github.io./",
+  "name": "KNOWN Group",
+}
+</script>
